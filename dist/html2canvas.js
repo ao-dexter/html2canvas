fc0f2242b9bdbd3894d5e171e5775090b2f00026
ao-dexter <dexter@athleticoffice.com>
2015-07-22 15:54:38 -0500

    Update html2canvas.js

--- dist/html2canvas.js
+++ dist/html2canvas.js
@@ -1940,6 +1940,7 @@ function html2canvas(nodeList, options) {
     if (options.logging) {
         window.html2canvas.logging = true;
         window.html2canvas.start = Date.now();
+        window.html2canvas.logmessage = '';
     }
 
     options.async = typeof(options.async) === "undefined" ? true : options.async;
@@ -2483,6 +2484,7 @@ module.exports = LinearGradientContainer;
 },{"./color":5,"./gradientcontainer":11}],15:[function(require,module,exports){
 module.exports = function() {
     if (window.html2canvas.logging && window.console && window.console.log) {
+    	window.html2canvas.logmessage = [].slice.call(arguments, 0);
         Function.prototype.bind.call(window.console.log, (window.console)).apply(window.console, [(Date.now() - window.html2canvas.start) + "ms", "html2canvas:"].concat([].slice.call(arguments, 0)));
     }
 };
